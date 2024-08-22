b63b03403145cbbbdea431d5f23dd0f558f9044e
Andrew Kozinsky <andkozinskiy@yandex.ru>
2024-08-22 12:56:23 +0500

    StyleLint was set up

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,26 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
+	env: {
+		// По умолчанию ESLint проверяет код по стандарту ES5.
+		// Поэтому конструкции типа const или let будут считаться ошибочными.
+		// Чтобы это исправить явно задаётся стандарт языка.
+		es2022: true,
+	},
+	// Требуется указывать такой парсер, чтобы ESLint не показывал ошибки при указании типов.
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		// Указание того, что код является модульным чтобы избежать ошибки
+		// ESLint: Parsing error: 'import' and 'export' may appear only with 'sourceType: module'
+		sourceType: 'module',
+	},
+	plugins: ['prettier'],
+	extends: ['plugin:prettier/recommended', 'prettier'],
+	rules: {
+		'prettier/prettier': 'error',
+		// Одинарные кавычки
+		quotes: ['warn', 'single'],
+		// Нельзя использовать var
+		'no-var': 'error',
+		// Предупреждение если переменная никогда не менялась
+		'prefer-const': 'warn',
+	},
 };
