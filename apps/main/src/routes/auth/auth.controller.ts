dbb978d7a95f084ca863602cd90bc04b857e1c95
Andrew Kozinsky <andkozinskiy@yandex.ru>
2024-09-13 08:31:49 +0500

    18

--- apps/main/src/routes/auth/auth.controller.ts
+++ apps/main/src/routes/auth/auth.controller.ts
@@ -94,12 +94,12 @@ export class AuthController {
 		@Body() body: CreateUserDtoModel,
 	): Promise<SWRegistrationRouteOut | undefined> {
 		try {
-			/*const commandRes = await this.commandBus.execute<
+			const commandRes = await this.commandBus.execute<
 				any,
 				ReturnType<typeof CreateUserHandler.prototype.execute>
 			>(new CreateUserCommand(body))
 
-			return createSuccessResp(routesConfig.registration, commandRes)*/
+			/*return createSuccessResp(routesConfig.registration, commandRes)*/
 			// ---
 			return {
 				status: 'success',
