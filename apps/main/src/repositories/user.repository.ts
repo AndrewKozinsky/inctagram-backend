5068adbdbe9758100b928feb016b31567200a449
Andrew Kozinsky <andkozinskiy@yandex.ru>
2024-09-14 14:13:25 +0500

    37

--- apps/main/src/repositories/user.repository.ts
+++ apps/main/src/repositories/user.repository.ts
@@ -127,15 +127,15 @@ export class UserRepository {
 		isEmailConfirmed = false,
 	) {
 		const newUserParams: any = {
-			email: dto.email,
-			name: dto.name,
-			hashed_password: await this.hashAdapter.hashString(dto.password),
-			email_confirmation_code: createUniqString(),
-			email_confirmation_code_expiration_date: add(new Date(), {
+			// email: dto.email,
+			// name: dto.name,
+			// hashed_password: await this.hashAdapter.hashString(dto.password),
+			// email_confirmation_code: createUniqString(),
+			/*email_confirmation_code_expiration_date: add(new Date(), {
 				hours: 0,
 				minutes: 5,
-			}).toISOString(),
-			is_email_confirmed: false,
+			}).toISOString(),*/
+			// is_email_confirmed: false,
 		}
 
 		// if (dto.githubId) {
