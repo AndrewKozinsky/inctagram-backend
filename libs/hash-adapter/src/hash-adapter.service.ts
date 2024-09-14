9b2ed13beed8c254448074815adca1a79bc360e1
Andrew Kozinsky <andkozinskiy@yandex.ru>
2024-09-14 15:12:49 +0500

    48

--- libs/hash-adapter/src/hash-adapter.service.ts
+++ libs/hash-adapter/src/hash-adapter.service.ts
@@ -4,21 +4,14 @@ import * as bcrypt from 'bcrypt'
 @Injectable()
 export class HashAdapterService {
 	generateSalt() {
-		try {
-			return bcrypt.genSalt()
-		} catch (err) {
-			console.log(err)
-		}
-		return 'dfgdgdddfg'
+		return bcrypt.genSalt()
 	}
 	generateHash(str: string, salt: string) {
 		return bcrypt.hash(str, salt)
 	}
 	async hashString(str: string) {
 		const passwordSalt = await this.generateSalt()
-		// return await this.generateHash(str, passwordSalt)
-		//---
-		return '4ererdgrdfverd'
+		return await this.generateHash(str, passwordSalt)
 	}
 	compare(str: string, hashedStr: string) {
 		return bcrypt.compare(str, hashedStr)
