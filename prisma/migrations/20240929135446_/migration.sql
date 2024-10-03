332f9524a40e859c965767c5c981819ac568b991
Andrew Kozinsky <andkozinskiy@yandex.ru>
2024-10-03 17:18:37 +0500

    Add week 3 migration

--- prisma/migrations/20240929135446_/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `country_id` on the `User` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "User" DROP COLUMN "country_id",
-ADD COLUMN     "country_code" TEXT;
--- prisma/migrations/20241003115335_week_3/migration.sql
+++ prisma/migrations/20241003115335_week_3/migration.sql
@@ -2,33 +2,21 @@
   Warnings:
 
   - You are about to drop the column `name` on the `User` table. All the data in the column will be lost.
-  - You are about to drop the `City` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `Country` table. If the table is not empty, all the data it contains will be lost.
   - A unique constraint covering the columns `[user_name]` on the table `User` will be added. If there are existing duplicate values, this will fail.
   - Added the required column `user_name` to the `User` table without a default value. This is not possible if the table is not empty.
 
 */
--- DropForeignKey
-ALTER TABLE "City" DROP CONSTRAINT "City_country_id_fkey";
-
 -- DropIndex
 DROP INDEX "User_name_key";
 
 -- AlterTable
-ALTER TABLE "User" DROP COLUMN "name",
+ALTER TABLE "User" RENAME COLUMN "name" TO "user_name",
 ADD COLUMN     "about_me" TEXT,
+ADD COLUMN     "avatar" TEXT,
 ADD COLUMN     "city_id" INTEGER,
-ADD COLUMN     "country_id" INTEGER,
-ADD COLUMN     "date_of_birth" TEXT,
+ADD COLUMN     "country_code" TEXT,
 ADD COLUMN     "first_name" TEXT,
 ADD COLUMN     "last_name" TEXT,
-ADD COLUMN     "user_name" TEXT NOT NULL;
-
--- DropTable
-DROP TABLE "City";
-
--- DropTable
-DROP TABLE "Country";
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_user_name_key" ON "User"("user_name");
