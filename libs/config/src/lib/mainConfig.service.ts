7fa1a1b717434289ca07f46fe7369fe197d25368
Andrew Kozinsky <andkozinskiy@yandex.ru>
2024-10-10 19:45:55 +0500

    Set secure parameter is cookies to false because requests make from localhost

--- libs/config/src/lib/mainConfig.service.ts
+++ libs/config/src/lib/mainConfig.service.ts
@@ -12,6 +12,7 @@ export class MainConfigService {
 				name: 'Inctagram',
 				domainApi: 'main.sociable-people.com',
 				domainApiWithPostfix: 'main.sociable-people.com/api/v1',
+				domainRoot: 'sociable-people.com',
 			},
 			db: {
 				host: this.configService.get<string>('DATABASE_URL') || '',
