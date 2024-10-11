9a02758e20bf44598050871b3d132ca7681d9c10
Andrew Kozinsky <andkozinskiy@yandex.ru>
2024-10-11 11:30:59 +0500

    A try to authorization with provider 6

--- apps/main/src/routes/auth/auth.controller.ts
+++ apps/main/src/routes/auth/auth.controller.ts
@@ -91,8 +91,12 @@ export class AuthController {
 		@Query() query: ProviderNameQueryModel,
 		@Query('code') providerCode: string,
 	) {
-		console.log({ req })
-		console.log(req.headers)
+		// console.log({ req })
+		// console.log(req.headers)
+		console.log(req.rawHeaders) // origin -> http://localhost:3000
+		console.log(typeof req.rawHeaders) // origin -> http://localhost:3000
+
+		// req.rawHeaders.find(header => header)
 		try {
 			const clientIP = this.browserService.getClientIP(req)
 			const clientName = this.browserService.getClientName(req)
